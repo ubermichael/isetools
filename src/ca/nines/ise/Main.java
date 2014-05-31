f13f9d99dc3369da42a1c679ea363fc257a7fa45
Michael Joyce <ubermichael@gmail.com>
2014-05-31 15:31:58 -0700

    first commit. rewrite of the perl ise tools in java.

---
+++ src/ca/nines/ise/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Michael Joyce <ubermichael@gmail.com>
+ */
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    String in = "Hello\nworld.";
+    Scanner s = new Scanner(in);
+    char c = s.getc();
+    while( ! s.finished()) {
+      System.out.println(s.line() + ":" + c);
+      c = s.getc();
+    }
+  }
+
+}
