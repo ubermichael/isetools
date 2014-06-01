b6dd24fb40fbeb3fa3ccd2ffd8bf00c9b0dea86d
Michael Joyce <ubermichael@gmail.com>
2014-05-31 21:15:33 -0700

    stub out node types.

---
+++ src/ca/nines/ise/node/TagNode.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.node;
+
+/**
+ *
+ * @author Michael Joyce <ubermichael@gmail.com>
+ */
+abstract public class TagNode extends Node {
+
+  @Override
+  String expanded() {
+    return getText();
+  }
+
+  @Override
+  String plain() {
+    return getText();
+  }
+
+  @Override
+  String unicode() {
+    return getText();
+  }
+
+}
