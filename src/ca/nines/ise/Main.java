d4cb01ea4aefe0363f2c881a098a48e61c46c978
Michael Joyce <ubermichael@gmail.com>
2014-06-01 18:50:54 -0700

    parsing tags correctly. woot.

--- src/ca/nines/ise/Main.java
+++ src/ca/nines/ise/Main.java
@@ -24,7 +24,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    String in = "{c}{P}{s}{^a}{^c}{_w}{ffl}";
+    String in = "hel<t n='3'>";
     ANTLRInputStream ais = new ANTLRInputStream(in);
     ISELexer lexer = new ISELexer(ais);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
