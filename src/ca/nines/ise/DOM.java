3a8f61006d6e70d7eef52178366e65738f87f5af
Michael Joyce <ubermichael@gmail.com>
2014-06-03 21:35:03 -0700

    Starting on the DOM bits.

---
+++ src/ca/nines/ise/DOM.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise;
+
+import ca.nines.ise.node.Node;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author Michael Joyce <ubermichael@gmail.com>
+ */
+public class DOM extends ArrayList<Node> {
+
+  public String toString() {
+    String s = "";
+    ListIterator<Node> i = this.listIterator();
+
+    while(i.hasNext()) {
+      Node n = i.next();
+      s += n + "\n";
+    }
+
+    return s;
+  }
+
+}
