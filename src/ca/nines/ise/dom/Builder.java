844138abcbafadf3ee10bded6b6879568f4da452
Michael Joyce <ubermichael@gmail.com>
2014-06-08 16:13:12 -0700

    cleanups.

--- src/ca/nines/ise/dom/Builder.java
+++ src/ca/nines/ise/dom/Builder.java
@@ -7,15 +7,34 @@ package ca.nines.ise.dom;
 
 import ca.nines.ise.grammar.ISELexer;
 import ca.nines.ise.grammar.ISEParser;
+import ca.nines.ise.grammar.ISEParser.AbbrContext;
+import ca.nines.ise.grammar.ISEParser.AttributeNameContext;
+import ca.nines.ise.grammar.ISEParser.AttributeValueContext;
+import ca.nines.ise.grammar.ISEParser.CharacterContext;
+import ca.nines.ise.grammar.ISEParser.CommentContext;
+import ca.nines.ise.grammar.ISEParser.ContentContext;
+import ca.nines.ise.grammar.ISEParser.EmptyTagContext;
+import ca.nines.ise.grammar.ISEParser.EndTagContext;
+import ca.nines.ise.grammar.ISEParser.StartTagContext;
+import ca.nines.ise.grammar.ISEParser.TagContext;
+import ca.nines.ise.grammar.ISEParser.TagNameContext;
 
-import ca.nines.ise.grammar.ISEParser.*;
 import ca.nines.ise.grammar.ISEParserBaseListener;
+import ca.nines.ise.node.AbbrNode;
+import ca.nines.ise.node.CharNode;
+import ca.nines.ise.node.CommentNode;
+import ca.nines.ise.node.EmptyNode;
+import ca.nines.ise.node.EndNode;
+import ca.nines.ise.node.Node;
+import ca.nines.ise.node.StartNode;
+import ca.nines.ise.node.TagNode;
+import ca.nines.ise.node.TextNode;
 
-import ca.nines.ise.node.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -67,56 +86,67 @@ public class Builder extends ISEParserBaseListener {
     return n;
   }
 
+  @Override
   public void enterAbbr(AbbrContext ctx) {
     AbbrNode n = (AbbrNode) setupNode(new AbbrNode(), ctx);
     dom.add(n);
   }
 
+  @Override
   public void enterCharacter(CharacterContext ctx) {
     CharNode n = (CharNode) setupNode(new CharNode(), ctx);
     dom.add(n);
   }
 
+  @Override
   public void enterContent(ContentContext ctx) {
     TextNode n = (TextNode) setupNode(new TextNode(), ctx);
     dom.add(n);
   }
 
+  @Override
   public void enterComment(CommentContext ctx) {
     CommentNode n = (CommentNode) setupNode(new CommentNode(), ctx);
     dom.add(n);
   }
 
+  @Override
   public void enterEndTag(EndTagContext ctx) {
     EndNode n = (EndNode) setupNode(new EndNode(), ctx);
     currentTag = n;
   }
 
+  @Override
   public void enterEmptyTag(EmptyTagContext ctx) {
     EmptyNode n = (EmptyNode) setupNode(new EmptyNode(), ctx);
     currentTag = n;
   }
 
+  @Override
   public void enterStartTag(StartTagContext ctx) {
     StartNode n = (StartNode) setupNode(new StartNode(), ctx);
     currentTag = n;
   }
 
+  @Override
   public void enterTagName(TagNameContext ctx) {
     String name = ctx.TAG_NAME().getText();
     currentTag.setName(name);
   }
 
+  @Override
   public void enterAttributeName(AttributeNameContext ctx) {
     currentAttrName = ctx.TAG_NAME().getText();
   }
 
+  @Override
   public void enterAttributeValue(AttributeValueContext ctx) {
     String value = ctx.ATTRIBUTE_VALUE().getText();
     currentTag.setAttribute(currentAttrName, value);
     currentAttrName = null;
   }
 
+  @Override
   public void exitTag(TagContext ctx) {
     dom.add(currentTag);
     currentTag = null;
