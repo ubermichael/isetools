6aaa273a2d469dc6b4497e534301679aca6cef1d
Michael Joyce <ubermichael@gmail.com>
2014-06-10 14:55:16 -0700

    find all abbrs.

--- src/ca/nines/ise/Abbrs.java
+++ src/ca/nines/ise/Abbrs.java
@@ -8,16 +8,17 @@ package ca.nines.ise;
 import ca.nines.ise.dom.Builder;
 import ca.nines.ise.dom.DOM;
 import ca.nines.ise.dom.DOMIterator;
-import ca.nines.ise.log.Log;
 import ca.nines.ise.node.Node;
-import ca.nines.ise.validator.TagValidator;
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
-import org.xml.sax.SAXException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
 /**
  *
@@ -26,19 +27,33 @@ import org.xml.sax.SAXException;
 public class Abbrs {
 
   public static void main(String[] args) {
-    try {
-      String filepath = "data/sgml/withTitlePage/Oth/Oth_F1.txt";
-      if(args.length == 1) {
-        filepath = args[0];
+
+  Collection<File> fileList = null;  
+  try {
+      if (args.length == 0) {
+        FileUtils fu = new FileUtils();
+        File dir = new File("data/sgml");        
+        SuffixFileFilter sfx = new SuffixFileFilter(".txt");
+        fileList = FileUtils.listFiles(dir, sfx, TrueFileFilter.INSTANCE);
+      } else {
+        for(String name : args) {
+          fileList = new ArrayList<>();
+          fileList.add(new File(name));
+        }
       }
-      File in = new File(filepath);
-      DOM dom = new Builder(in).getDOM();
-      DOMIterator i = dom.getIterator();
-      while(i.hasNext()) {
-        Node n = i.next();
-        System.out.println(n);
-        if(n.type() == "#ABBR") {
-          System.out.println(n);
+      
+      System.out.println("Found " + fileList.size() + " files to check.");
+      
+      Iterator fi = fileList.iterator();
+      while(fi.hasNext()) {
+        File in = (File) fi.next();
+        DOM dom = new Builder(in).getDOM();
+        DOMIterator i = dom.getIterator();
+        while (i.hasNext()) {
+          Node n = i.next();
+          if (n.type() == "#ABBR") {
+            System.out.println(n);
+          }
         }
       }
     } catch (Exception ex) {
