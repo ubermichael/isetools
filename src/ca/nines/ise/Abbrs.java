5af7f6931748cd1703f7e516acf6adb8247b9366
Michael Joyce <ubermichael@gmail.com>
2014-06-10 00:15:22 -0700

    start hunting down abbrs.

--- src/ca/nines/ise/Abbrs.java
+++ src/ca/nines/ise/Abbrs.java
@@ -7,7 +7,9 @@ package ca.nines.ise;
 
 import ca.nines.ise.dom.Builder;
 import ca.nines.ise.dom.DOM;
+import ca.nines.ise.dom.DOMIterator;
 import ca.nines.ise.log.Log;
+import ca.nines.ise.node.Node;
 import ca.nines.ise.validator.TagValidator;
 import java.io.File;
 import java.io.IOException;
@@ -21,23 +23,26 @@ import org.xml.sax.SAXException;
  *
  * @author Michael Joyce <michael@negativespace.net>
  */
-public class Main {
+public class Abbrs {
 
   public static void main(String[] args) {
     try {
       String filepath = "data/sgml/withTitlePage/Oth/Oth_F1.txt";
-
       if(args.length == 1) {
         filepath = args[0];
       }
       File in = new File(filepath);
       DOM dom = new Builder(in).getDOM();
-      TagValidator tv = new TagValidator();
-      Log log = Log.getInstance();
-      tv.validate(dom);
-      System.out.println(log);
+      DOMIterator i = dom.getIterator();
+      while(i.hasNext()) {
+        Node n = i.next();
+        System.out.println(n);
+        if(n.type() == "#ABBR") {
+          System.out.println(n);
+        }
+      }
     } catch (Exception ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      Logger.getLogger(Abbrs.class.getName()).log(Level.SEVERE, null, ex);
     }
   }
 
