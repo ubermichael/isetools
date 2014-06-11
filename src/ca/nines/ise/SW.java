a7a53954121ba4991b1a337ff4e7bda8cb6a0047
Michael Joyce <ubermichael@gmail.com>
2014-06-11 14:20:19 -0700

    better schema2wiki output.

--- src/ca/nines/ise/SW.java
+++ src/ca/nines/ise/SW.java
@@ -5,68 +5,130 @@
  */
 package ca.nines.ise;
 
-import ca.nines.ise.dom.Builder;
-import ca.nines.ise.dom.DOM;
-import ca.nines.ise.log.Log;
-import ca.nines.ise.validator.TagValidator;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+import ca.nines.ise.schema.Attribute;
+import ca.nines.ise.schema.Schema;
+import ca.nines.ise.schema.Tag;
+import java.util.Arrays;
+import java.util.Formatter;
 import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.SuffixFileFilter;
-import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.xml.sax.SAXException;
 
 /**
  *
  * @author Michael Joyce <michael@negativespace.net>
  */
-public class Main {
+public class SW {
+
+  public static void row(StringBuilder sb) {
+    sb.append("|------------\n");
+  }
+
+  public static void tagRow(Formatter fmt, Tag tag) {
+    fmt.format("|| %s || %s || %s || %s ||%n",
+            tag.getName(),
+            tag.getEmpty(),
+            tag.getWhere(),
+            tag.getDepreciated());
+  }
+
+  public static void attrHeader(StringBuilder sb) {
+    sb.append("{{{#!td\n"
+            + "}}}\n"
+            + "{{{#!td colspan=3\n"
+            + "|| Name || Type || Optional || Emtpy || Match || Renmberable || Options ||\n"
+    );
+  }
+
+  public static String attrOptions(Attribute attr) {
+    StringBuilder sb = new StringBuilder();
+    String[] options = attr.getOptions();
+    if (options.length == 0) {
+      return "";
+    }
+    Iterator i = Arrays.asList(options).iterator();
+    while (i.hasNext()) {
+      String s = (String) i.next();
+      sb.append(s);
+      if(i.hasNext()) {
+        sb.append(", ");
+      }
+    }
+    return sb.toString();
+  }
+
+  public static void attrRow(Formatter fmt, Attribute attr) {
+    fmt.format("|| @''%s'' || %s || %s || %s || %s || %s || %s ||%n",
+            attr.getName(),
+            attr.getType(),
+            attr.getOptional(),
+            attr.getEmpty(),
+            attr.getMatch(),
+            attr.getRenumber(),
+            attrOptions(attr));
+  }
 
   public static void main(String[] args) {
-    Collection<File> fileList = null;
     try {
-      if (args.length == 0) {
-        FileUtils fu = new FileUtils();
-        File dir = new File("data/sgml");
-        SuffixFileFilter sfx = new SuffixFileFilter(".txt");
-        fileList = FileUtils.listFiles(dir, sfx, TrueFileFilter.INSTANCE);
-      } else {
-        for (String name : args) {
-          fileList = new ArrayList<>();
-          fileList.add(new File(name));
+      Schema schema = new Schema();
+      StringBuilder sb = new StringBuilder();
+      Formatter fmt = new Formatter(sb);
+
+      sb.append("||= Name =||= Empty =||= Context =||= Depreciated =||\n");
+      row(sb);
+
+      for (String tagName : schema.getTagNames()) {
+        Tag tag = schema.getTag(tagName);
+        tagRow(fmt, tag);
+        row(sb);
+
+        if (tag.countAttributes() > 0) {
+          attrHeader(sb);
+          row(sb);
+          for (String attrName : tag.getAttributeNames()) {
+            Attribute attr = tag.getAttribute(attrName);
+            attrRow(fmt, attr);
+          }
+          sb.append("}}}\n");
+          row(sb);
         }
       }
-
-      System.out.println("Found " + fileList.size() + " files to check.");
-
-      Iterator fi = fileList.iterator();
-      while (fi.hasNext()) {
-        File in = (File) fi.next();
-        DOM dom = new Builder(in).getDOM();
-        TagValidator tv = new TagValidator();
-        Log log = Log.getInstance();
-        tv.validate(dom);
-        System.out.print(log);
-        log.clear();
-      }
-    } catch (IOException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (ParserConfigurationException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (SAXException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (XPathExpressionException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      System.out.println(sb);
     } catch (Exception ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      Logger.getLogger(SW.class.getName()).log(Level.SEVERE, null, ex);
     }
 
   }
 }
+
+/*
+
+ ||= Name =||= Empty =||= Context =||= Depreciated =||
+ |------
+ || '''ABBR''' || no ||  ||  ||
+ |------
+ {{{#!th
+ }}}
+ {{{#!th colspan=3
+ || Name || Type || Optional || Emtpy || Match || Renmberable || Options ||
+ }}}
+ |------
+ {{{#!td
+ }}}
+ {{{#!td colspan=3
+ || @''expan'' || string || null || null || null || null ||  ||
+ || @''expan'' || string || null || null || null || null ||  ||
+ }}}
+ |------
+ || '''ACT''' || no ||  ||  ||
+ |------
+ {{{#!th
+ }}}
+ {{{#!th colspan=3
+ || Name || Type || Optional || Emtpy || Match || Renmberable || Options ||
+ || @''expan'' || string || null || null || null || null ||  ||
+ || @''expan'' || string || null || null || null || null ||  ||
+ }}}
+
+ */
