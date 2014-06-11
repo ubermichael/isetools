c7ae624b62230c9c9bede06041bed5d0128ed2c6
Michael Joyce <ubermichael@gmail.com>
2014-06-11 14:19:17 -0700

    still no idea how to test a grammar. Start turning the schema into something wikifiable.

---
+++ src/ca/nines/ise/SW.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.nines.ise;
+
+import ca.nines.ise.dom.Builder;
+import ca.nines.ise.dom.DOM;
+import ca.nines.ise.log.Log;
+import ca.nines.ise.validator.TagValidator;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Michael Joyce <ubermichael@gmail.com>
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Collection<File> fileList = null;
+    try {
+      if (args.length == 0) {
+        FileUtils fu = new FileUtils();
+        File dir = new File("data/sgml");
+        SuffixFileFilter sfx = new SuffixFileFilter(".txt");
+        fileList = FileUtils.listFiles(dir, sfx, TrueFileFilter.INSTANCE);
+      } else {
+        for (String name : args) {
+          fileList = new ArrayList<>();
+          fileList.add(new File(name));
+        }
+      }
+
+      System.out.println("Found " + fileList.size() + " files to check.");
+
+      Iterator fi = fileList.iterator();
+      while (fi.hasNext()) {
+        File in = (File) fi.next();
+        DOM dom = new Builder(in).getDOM();
+        TagValidator tv = new TagValidator();
+        Log log = Log.getInstance();
+        tv.validate(dom);
+        System.out.print(log);
+        log.clear();
+      }
+    } catch (IOException ex) {
+      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (ParserConfigurationException ex) {
+      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (SAXException ex) {
+      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (XPathExpressionException ex) {
+      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (Exception ex) {
+      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+  }
+}
