d603fda339c3e150391ee11a00cf705a95f8d360
Michael Joyce <ubermichael@gmail.com>
2014-06-17 22:40:16 -0700

    stuff to try out.

--- src/ca/nines/ise/Tester.java
+++ src/ca/nines/ise/Tester.java
@@ -3,31 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ca.nines.ise;
 
-import ca.nines.ise.util.XMLReader;
+import ca.nines.ise.dom.Builder;
+import ca.nines.ise.dom.DOM;
+import ca.nines.ise.log.Log;
+import ca.nines.ise.log.Message;
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.w3c.dom.Node;
 
 /**
  *
  * @author michael
  */
 public class Tester {
+
   public static void main(String[] args) {
+    Log log = Log.getInstance();
     try {
-      File file = new File("/resources/schemas/default.xml");
-      XMLReader xmlIn = new XMLReader(file);
-      Node[] nl = xmlIn.xpathList("//tag");
-      System.out.println("found " + nl.length);
-      for(Node n : nl) {
-        System.out.println(n.getNodeName());
-      }
+      DOM dom = new Builder(new File("foo")).getDOM();
+      System.out.println(dom);
     } catch (Exception ex) {
-      Logger.getLogger(Tester.class.getName()).log(Level.SEVERE, null, ex);
+      Message m = log.add("unknown");
+      m.addNote(ex.getMessage());
     }
+    System.out.println(log);
   }
 }
