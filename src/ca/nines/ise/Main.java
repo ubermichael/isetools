ef4f7f194a1b2fdfece4111e2422aa45636f8d64
Michael Joyce <ubermichael@gmail.com>
2014-06-21 13:09:24 -0700

    output encoded utf8.

--- src/ca/nines/ise/Main.java
+++ src/ca/nines/ise/Main.java
@@ -10,18 +10,14 @@ import ca.nines.ise.dom.DOM;
 import ca.nines.ise.log.Log;
 import ca.nines.ise.validator.TagValidator;
 import java.io.File;
-import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
+import java.util.Locale;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -31,7 +27,12 @@ public class Main {
 
   public static void main(String[] args) {
     Collection<File> fileList = null;
+    Log log = Log.getInstance();
+    Locale.setDefault(Locale.ENGLISH);
+    PrintStream out = null;
+
     try {
+      out = new PrintStream(System.out, true, "UTF-8");
       if (args.length == 0) {
         FileUtils fu = new FileUtils();
         File dir = new File("data/sgml");
@@ -44,28 +45,19 @@ public class Main {
         }
       }
 
-      System.out.println("Found " + fileList.size() + " files to check.");
+      out.println("Found " + fileList.size() + " files to check.");
 
       Iterator fi = fileList.iterator();
       while (fi.hasNext()) {
         File in = (File) fi.next();
         DOM dom = new Builder(in).getDOM();
         TagValidator tv = new TagValidator();
-        Log log = Log.getInstance();
         tv.validate(dom);
-        System.out.print(log);
+        out.println(log);
         log.clear();
       }
-    } catch (IOException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (ParserConfigurationException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (SAXException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (XPathExpressionException ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
     } catch (Exception ex) {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      ex.printStackTrace(out);
     }
   }
 }
