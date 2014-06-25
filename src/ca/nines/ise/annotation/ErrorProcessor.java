8e00b515d979405ab19b88497835ae8132741950
Michael Joyce <ubermichael@gmail.com>
2014-06-24 21:48:44 -0700

    some ideas.

---
+++ src/ca/nines/ise/annotation/ErrorProcessor.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.annotation;
+
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+/**
+ *
+ * @author michael
+ */
+@SupportedAnnotationTypes({
+  "ErrorCode"
+})
+public class ErrorProcessor extends AbstractProcessor {
+
+  @Override
+  public SourceVersion getSupportedSourceVersion() {
+    return SourceVersion.latestSupported();    
+  }
+  
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    Messager messager = processingEnv.getMessager();
+    for(TypeElement typeElement : annotations) {
+      for(Element element : roundEnv.getElementsAnnotatedWith(typeElement)) {
+        messager.printMessage(Diagnostic.Kind.NOTE, "ErrorCode: " + element.toString());
+      }
+    }
+    return true;
+  }
+  
+}
---
+++ src/ca/nines/ise/cmd/Command.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.cmd;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author michael
+ */
+public class Command {
+  
+  protected static final HashMap<String, Class<? extends Command>> commandList = new HashMap<>();
+  
+}
