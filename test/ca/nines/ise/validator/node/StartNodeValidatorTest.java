d603eac2ccc6ce43e4c2413d79a6ab0163f9d8ba
Michael Joyce <ubermichael@gmail.com>
2014-06-25 13:41:16 -0700

    stubs for testing.

---
+++ test/ca/nines/ise/validator/node/StartNodeValidatorTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.validator.node;
+
+import ca.nines.ise.node.StartNode;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author michael
+ */
+public class StartNodeValidatorTest {
+  
+  public StartNodeValidatorTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+  
+  @AfterClass
+  public static void tearDownClass() {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
+
+  /**
+   * Test of validate method, of class StartNodeValidator.
+   */
+  @Test
+  public void testValidate() {
+//    System.out.println("validate");
+//    StartNode n = null;
+//    StartNodeValidator instance = null;
+//    instance.validate(n);
+//    // TODO review the generated test code and remove the default call to fail.
+//    fail("The test case is a prototype.");
+  }
+  
+}
