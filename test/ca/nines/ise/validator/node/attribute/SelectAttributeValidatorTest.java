fe3ce311d4695495f3d42fbacf15a07d46955559
Michael Joyce <ubermichael@gmail.com>
2014-06-25 17:41:58 -0700

    All attribute and tag validations are now tested.

---
+++ test/ca/nines/ise/validator/node/attribute/SelectAttributeValidatorTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.validator.node.attribute;
+
+import ca.nines.ise.node.TagNode;
+import ca.nines.ise.schema.Attribute;
+import ca.nines.ise.validator.node.TagNodeImpl;
+import ca.nines.ise.validator.node.ValidatorTestBase;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author michael
+ */
+public class SelectAttributeValidatorTest extends ValidatorTestBase {
+  
+  public SelectAttributeValidatorTest() throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+    super();
+  }
+  
+  /**
+   * Test of validate method, of class SelectAttributeValidator.
+   */
+  @Test
+  public void testValidate() {
+    TagNode n = new TagNodeImpl();
+    n.setName("SEL");
+    Attribute attr = schema.getTag(n.getName()).getAttribute("opt");    
+    SelectAttributeValidator instance = new SelectAttributeValidator();    
+    
+    n.setAttribute("opt", "thyme");
+    instance.validate(n, attr);
+    checkLog(new String[]{});
+
+    n.setAttribute("opt", "mint");
+    instance.validate(n, attr);
+    checkLog(new String[]{});
+
+    n.setAttribute("opt", "basil");
+    instance.validate(n, attr);
+    checkLog(new String[]{});
+
+    n.setAttribute("opt", "minty");
+    instance.validate(n, attr);
+    checkLog(new String[]{"validator.attribute.badselect"});
+
+    n.setAttribute("opt", "");
+    instance.validate(n, attr);
+    checkLog(new String[]{"validator.attribute.badselect"});
+  }
+  
+}
