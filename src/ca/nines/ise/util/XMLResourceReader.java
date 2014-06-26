4ac65d122b7c5ce840638facbc8b522eb96f9f5d
Michael Joyce <ubermichael@gmail.com>
2014-06-26 11:08:48 -0700

    split out file/resource xml reader. because jar files can't do FileInputStreams.

--- src/ca/nines/ise/util/XMLResourceReader.java
+++ src/ca/nines/ise/util/XMLResourceReader.java
@@ -8,19 +8,9 @@ package ca.nines.ise.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import org.apache.commons.io.IOUtils;
-import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -32,18 +22,10 @@ import org.xml.sax.SAXException;
  * <p>
  * @author michael
  */
-public class XMLResourceReader {
-
-  private final Node root;
-  private final DocumentBuilderFactory factory;
-  private final DocumentBuilder builder;
-  private final XPathFactory xpfactory;
-  private final XPath xpath;
-  private String source;
+public class XMLResourceReader extends XMLReader {
 
   public XMLResourceReader(String in) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
-    this(IOUtils.toInputStream(in, "UTF-8"));
-    source = "#STRING";
+    super(in);
   }
 
   public XMLResourceReader(File in) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
@@ -52,62 +34,10 @@ public class XMLResourceReader {
   }
 
   public XMLResourceReader(InputStream stream) throws SAXException, IOException, XPathExpressionException, ParserConfigurationException {
-    factory = DocumentBuilderFactory.newInstance();
-    builder = factory.newDocumentBuilder();
-    xpfactory = XPathFactory.newInstance();
-    xpath = xpfactory.newXPath();
-    source = "#STREAM";
-
-    Document doc = builder.parse(stream);
-    XPathExpression expr = xpath.compile("/node()");
-    root = (Node) expr.evaluate(doc, XPathConstants.NODE);
+    super(stream);
   }
 
   public XMLResourceReader(Node in) throws ParserConfigurationException {
-    factory = null;
-    builder = null;
-    xpfactory = XPathFactory.newInstance();
-    xpath = xpfactory.newXPath();
-    source = "#NODE";
-
-    root = in;
-  }
-
-  public Node[] xpathList(String xp) throws XPathExpressionException {
-    return xpathList(xp, root);
-  }
-
-  public Node[] xpathList(String xp, Node node) throws XPathExpressionException {
-    ArrayList<Node> nodes = new ArrayList<>();
-    XPathExpression expr = xpath.compile(xp);
-    NodeList nl = (NodeList) expr.evaluate(node, XPathConstants.NODESET);
-    int length = nl.getLength();
-    for (int i = 0; i < length; i++) {
-      nodes.add(nl.item(i));
-    }
-    return nodes.toArray(new Node[nodes.size()]);
-  }
-
-  public Node xpathNode(String xp) throws XPathExpressionException {
-    return xpathNode(xp, root);
-  }
-
-  public Node xpathNode(String xp, Node node) throws XPathExpressionException {
-    XPathExpression expr = xpath.compile(xp);
-    Node n = (Node) expr.evaluate(node, XPathConstants.NODE);
-    return n;
-  }
-
-  public String xpathString(String xp) throws XPathExpressionException {
-    return xpathString(xp, root);
-  }
-
-  public String xpathString(String xp, Node node) throws XPathExpressionException {
-    XPathExpression expr = xpath.compile(xp);
-    return (String) expr.evaluate(node, XPathConstants.STRING);
-  }
-
-  public String getSource() {
-    return source;
+    super(in);
   }
 }
