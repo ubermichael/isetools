c4d3bf45971cefce137b0169f92cfc0508b1378e
Michael Joyce <ubermichael@gmail.com>
2014-06-26 22:35:08 -0700

    Accented chars tested and bug fixed. whoa.

--- test/ca/nines/ise/node/chr/AccentCharNodeTest.java
+++ test/ca/nines/ise/node/chr/AccentCharNodeTest.java
@@ -6,11 +6,12 @@
 package ca.nines.ise.node.chr;
 
 import ca.nines.ise.dom.Fragment;
-import ca.nines.ise.node.CharNode;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import ca.nines.ise.node.Node;
+import ca.nines.ise.node.StartNode;
+import ca.nines.ise.validator.node.TestBase;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.Iterator;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,53 +19,194 @@ import static org.junit.Assert.*;
  *
  * @author michael
  */
-public class AccentCharNodeTest {
-
-  public AccentCharNodeTest() {
-  }
-
-  @BeforeClass
-  public static void setUpClass() {
-  }
-
-  @AfterClass
-  public static void tearDownClass() {
-  }
-
-  @Before
-  public void setUp() {
-  }
-
-  @After
-  public void tearDown() {
-  }
+public class AccentCharNodeTest extends TestBase {
 
   /**
-   * Test of getCharType method, of class AccentCharNode.
+   * Test of expanded method, of class AccentCharNode.
    */
   @Test
-  public void testGetCharType() {
-    System.out.println("getCharType");
+  public void testExpandedCircumflex() {
     AccentCharNode instance = new AccentCharNode();
-    CharNode.CharType expResult = null;
-    CharNode.CharType result = instance.getCharType();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    instance.setText("{^e}");
+    Fragment dom = instance.expanded();
+    Iterator<Node> iterator = dom.iterator();
+    Node n;
+    
+    assertEquals(3, dom.size());
+    n = iterator.next();
+    assertEquals("START", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    assertEquals("{^e}", ((StartNode)n).getAttribute("setting"));
+    
+    n = iterator.next();
+    assertEquals("TEXT", n.type().name());
+    assertEquals(Normalizer.normalize("e\u0302", Form.NFC), n.getText());
+    
+    n = iterator.next();
+    assertEquals("END", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    checkLog();
   }
 
   /**
    * Test of expanded method, of class AccentCharNode.
    */
   @Test
-  public void testExpanded() {
-    System.out.println("expanded");
+  public void testExpandedUmlat() {
     AccentCharNode instance = new AccentCharNode();
-    Fragment expResult = null;
-    Fragment result = instance.expanded();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    instance.setText("{\"u}");
+    Fragment dom = instance.expanded();
+    Iterator<Node> iterator = dom.iterator();
+    Node n;
+    
+    assertEquals(3, dom.size());
+    n = iterator.next();
+    assertEquals("START", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    assertEquals("{\"u}", ((StartNode)n).getAttribute("setting"));
+    
+    n = iterator.next();
+    assertEquals("TEXT", n.type().name());
+    assertEquals(Normalizer.normalize("ü", Form.NFC), n.getText());
+    
+    n = iterator.next();
+    assertEquals("END", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    checkLog();
   }
 
+  /**
+   * Test of expanded method, of class AccentCharNode.
+   */
+  @Test
+  public void testExpandedAcute() {
+    AccentCharNode instance = new AccentCharNode();
+    instance.setText("{'a}");
+    Fragment dom = instance.expanded();
+    Iterator<Node> iterator = dom.iterator();
+    Node n;
+    
+    assertEquals(3, dom.size());
+    n = iterator.next();
+    assertEquals("START", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    assertEquals("{'a}", ((StartNode)n).getAttribute("setting"));
+    
+    n = iterator.next();
+    assertEquals("TEXT", n.type().name());
+    assertEquals(Normalizer.normalize("á", Form.NFC), n.getText());
+    
+    n = iterator.next();
+    assertEquals("END", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    checkLog();
+  }
+
+  /**
+   * Test of expanded method, of class AccentCharNode.
+   */
+  @Test
+  public void testExpandedGrave() {
+    AccentCharNode instance = new AccentCharNode();
+    instance.setText("{`o}");
+    Fragment dom = instance.expanded();
+    Iterator<Node> iterator = dom.iterator();
+    Node n;
+    
+    assertEquals(3, dom.size());
+    n = iterator.next();
+    assertEquals("START", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    assertEquals("{`o}", ((StartNode)n).getAttribute("setting"));
+    
+    n = iterator.next();
+    assertEquals("TEXT", n.type().name());
+    assertEquals(Normalizer.normalize("ò", Form.NFC), n.getText());
+    
+    n = iterator.next();
+    assertEquals("END", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    checkLog();
+  }
+
+  /**
+   * Test of expanded method, of class AccentCharNode.
+   */
+  @Test
+  public void testExpandedMacron() {
+    AccentCharNode instance = new AccentCharNode();
+    instance.setText("{_o}");
+    Fragment dom = instance.expanded();
+    Iterator<Node> iterator = dom.iterator();
+    Node n;
+    
+    assertEquals(3, dom.size());
+    n = iterator.next();
+    assertEquals("START", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    assertEquals("{_o}", ((StartNode)n).getAttribute("setting"));
+    
+    n = iterator.next();
+    assertEquals("TEXT", n.type().name());
+    assertEquals(Normalizer.normalize("ō", Form.NFC), n.getText());
+    
+    n = iterator.next();
+    assertEquals("END", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    checkLog();
+  }
+
+  /**
+   * Test of expanded method, of class AccentCharNode.
+   */
+  @Test
+  public void testExpandedTilde() {
+    AccentCharNode instance = new AccentCharNode();
+    instance.setText("{~n}");
+    Fragment dom = instance.expanded();
+    Iterator<Node> iterator = dom.iterator();
+    Node n;
+    
+    assertEquals(3, dom.size());
+    n = iterator.next();
+    assertEquals("START", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    assertEquals("{~n}", ((StartNode)n).getAttribute("setting"));
+    
+    n = iterator.next();
+    assertEquals("TEXT", n.type().name());
+    assertEquals(Normalizer.normalize("ñ", Form.NFC), n.getText());
+    
+    n = iterator.next();
+    assertEquals("END", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    checkLog();
+  }
+
+  /**
+   * Test of expanded method, of class AccentCharNode.
+   */
+  @Test
+  public void testExpandedBad() {
+    AccentCharNode instance = new AccentCharNode();
+    instance.setText("{!e}");
+    Fragment dom = instance.expanded();
+    Iterator<Node> iterator = dom.iterator();
+    Node n;
+    
+    assertEquals(3, dom.size());
+    n = iterator.next();
+    assertEquals("START", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    assertEquals("{!e}", ((StartNode)n).getAttribute("setting"));
+    
+    n = iterator.next();
+    assertEquals("TEXT", n.type().name());
+    assertEquals(Normalizer.normalize("e\uFFFD", Form.NFC), n.getText());
+    
+    n = iterator.next();
+    assertEquals("END", n.type().name());
+    assertEquals("ACCENT", n.getName());
+    checkLog(new String[]{"char.accent.unknown"});
+  }
 }
