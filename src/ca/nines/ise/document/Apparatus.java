926f65aedc21a2b0c5f18b06d82ba9633ee3bd76
Michael Joyce <ubermichael@gmail.com>
2014-06-26 17:58:29 -0700

    And the annotations.

---
+++ src/ca/nines/ise/document/Apparatus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.document;
+
+import ca.nines.ise.node.lemma.Lemma;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author michael
+ * @param <T>
+ */
+abstract public class Apparatus<T extends Lemma> {
+  
+  ArrayList<T> lemmas;
+  
+}
