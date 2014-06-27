dd940a989808baa141c87f7ae8e222f1b2251e8e
Michael Joyce <ubermichael@gmail.com>
2014-06-26 17:58:29 -0700

    And the annotations.

---
+++ src/ca/nines/ise/document/Annotations.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.document;
+
+import ca.nines.ise.node.lemma.Note;
+
+/**
+ *
+ * @author michael
+ */
+public class Annotations extends Apparatus<Note> {
+}
---
+++ src/ca/nines/ise/node/lemma/Lemma.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.node.lemma;
+
+/**
+ *
+ * @author michael
+ */
+abstract public class Lemma {
+  
+}
