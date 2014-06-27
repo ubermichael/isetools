0ec95ec57d943f5e99a36671733811f3446a8914
Michael Joyce <ubermichael@gmail.com>
2014-06-26 17:57:57 -0700

    I don’t know that i’ll need this. but it’s a good file finding utility.

---
+++ src/ca/nines/ise/util/FileFinder.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.nines.ise.util;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Pattern;
+import org.apache.commons.io.DirectoryWalker;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+/**
+ *
+ * @author michael
+ */
+public class FileFinder extends DirectoryWalker {
+
+  FileFilter filter;
+  
+  @Override
+  protected boolean handleDirectory(File directory, int depth, Collection results) throws IOException {
+    return HiddenFileFilter.VISIBLE.accept(directory);
+  }
+  
+  @Override
+  protected void handleFile(File file, int depth, Collection results) throws IOException {
+    if(filter.accept(file)) {
+      results.add(file);
+    }
+  }
+  
+  public ArrayList find(File startDirectory, String wildcard) throws IOException {
+    ArrayList<File> files = new ArrayList<>();
+    filter = new WildcardFileFilter(wildcard);
+    walk(startDirectory, files);
+    return files;
+  }
+  
+  public ArrayList<File> find(File startDirectory, Pattern matcher) throws IOException {
+    ArrayList<File> files = new ArrayList<>();
+    filter = new RegexFileFilter(matcher);
+    walk(startDirectory, files);
+    return files;
+  }
+  
+}
