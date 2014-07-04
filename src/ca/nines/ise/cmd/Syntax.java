d1d20d6a92fc2ec27e686e4710c6c6744cbda906
Michael Joyce <ubermichael@gmail.com>
2014-07-04 15:51:22 -0700

    Now a syntax checker.

--- src/ca/nines/ise/cmd/Syntax.java
+++ src/ca/nines/ise/cmd/Syntax.java
@@ -8,8 +8,6 @@ package ca.nines.ise.cmd;
 import ca.nines.ise.dom.Builder;
 import ca.nines.ise.dom.DOM;
 import ca.nines.ise.log.Log;
-import ca.nines.ise.schema.Schema;
-import ca.nines.ise.validator.DOMValidator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -22,24 +20,21 @@ import java.util.List;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.xml.sax.SAXException;
 
 /**
  *
- * @author michael
+ * @author Michael Joyce <ubermichael@gmail.com>
  */
-public class Validate extends Command {
+public class Syntax extends Command {
 
   @Override
   public String description() {
-    return "Validate one or more ISE SGML documents.";
+    return "Syntax check one or more ISE SGML documents.";
   }
 
   @Override
@@ -56,8 +51,6 @@ public class Validate extends Command {
 
       Log log = Log.getInstance();
       Locale.setDefault(Locale.ENGLISH);
-      Schema schema = new Schema();
-      DOMValidator validator = new DOMValidator(schema);
       PrintStream out = new PrintStream(System.out, true, "UTF-8");
 
       if (cmd.hasOption("l")) {
@@ -66,7 +59,7 @@ public class Validate extends Command {
 
       List<String> argList = cmd.getArgList();
       argList = argList.subList(1, argList.size());
-      
+
       if (argList.isEmpty()) {
         File dir = new File("input");
         SuffixFileFilter sfx = new SuffixFileFilter(".txt");
@@ -80,9 +73,10 @@ public class Validate extends Command {
         out.println("Found " + fileList.size() + " files to check.");
         for (File in : fileList) {
           DOM dom = new Builder(in).getDOM();
-          validator.validate(dom);
-          out.println(log);
-          log.clear();
+          if (log.count() > 0) {
+            out.println(log);
+            log.clear();
+          }
         }
       }
 
@@ -92,14 +86,7 @@ public class Validate extends Command {
       Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
     } catch (IOException ex) {
       Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (ParserConfigurationException ex) {
-      Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (SAXException ex) {
-      Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (XPathExpressionException ex) {
-      Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (Exception ex) {
-      Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
     }
   }
+
 }
