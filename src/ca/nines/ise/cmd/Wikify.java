9726b36424d705f519e8d1379c564f532363015d
Michael Joyce <ubermichael@gmail.com>
2014-07-06 11:45:49 -0700

    thoughts on generating wiki pages.

--- src/ca/nines/ise/cmd/Wikify.java
+++ src/ca/nines/ise/cmd/Wikify.java
@@ -5,18 +5,10 @@
  */
 package ca.nines.ise.cmd;
 
-import ca.nines.ise.dom.Builder;
-import ca.nines.ise.dom.DOM;
-import ca.nines.ise.node.CharNode;
-import ca.nines.ise.node.Node;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,17 +19,16 @@ import org.apache.commons.cli.Options;
  *
  * @author Michael Joyce <michael@negativespace.net>
  */
-public class Chars extends Command {
+public class Wikify extends Command {
 
   @Override
   public String description() {
-    return "Report on the special characters used in one or more documents.";
+    return "Create a wikified version of the schema.";
   }
 
   @Override
   public void execute(CommandLine cmd) {
     try {
-      File[] files;
       Locale.setDefault(Locale.ENGLISH);
       PrintStream out = new PrintStream(System.out, true, "UTF-8");
 
@@ -45,44 +36,7 @@ public class Chars extends Command {
         out = new PrintStream(new FileOutputStream(cmd.getOptionValue("l")), true, "UTF-8");
       }
 
-      HashMap<String, Integer> m = new HashMap<>();
-      HashMap<CharNode.CharType, HashMap<String, Integer>> types = new HashMap<>();
-      files = getFilePaths(cmd);
-
-      for (CharNode.CharType type : CharNode.CharType.values()) {
-        types.put(type, new HashMap<String, Integer>());
-      }
-
-      out.println("Found " + files.length + " files to check.");
-      for (File file : files) {
-        DOM dom = new Builder(file).getDOM();
-        Iterator<Node> i = dom.iterator();
-        while (i.hasNext()) {
-          Node n = i.next();
-          if (n instanceof CharNode) {
-            CharNode c = (CharNode) n;
-            m.put(n.getText(), 1);
-            types.get(c.getCharType()).put(c.getText(), 1);
-          }
-        }
-      }
-
-      out.println("EXISTING CHARS BY TYPE:");
-      for (CharNode.CharType type : CharNode.CharType.values()) {
-        out.println(type.name());
-        for (String str : types.get(type).keySet()) {
-          out.println(str);
-        }
-      }
-
-      out.println("EXISTING CHARS: ");
-      for (String k : m.keySet()) {
-        out.println(k);
-      }
-
     } catch (UnsupportedEncodingException ex) {
-      Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (FileNotFoundException ex) {
       Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
     } catch (IOException ex) {
       Logger.getLogger(Validate.class.getName()).log(Level.SEVERE, null, ex);
@@ -92,8 +46,14 @@ public class Chars extends Command {
   @Override
   public Options getOptions() {
     Options opts = new Options();
-    opts.addOption("l", true, "Send output to log file");
+    opts.addOption("o", true, "Send output to file");
+    opts.addOption("L", false, "Show a list of locations");
+    opts.addOption("l", true, "Generate output for specific location");
     return opts;
   }
 
+  @Override
+  public String getUsage() {
+    return "[options]";
+  }
 }
