5a4b05b3254aaa0d0d5768b60e58f66f296d71c7
Michael Joyce <ubermichael@gmail.com>
2014-07-12 14:48:23 -0700

    not bad. not great either.

--- src/ca/nines/ise/Tester.java
+++ src/ca/nines/ise/Tester.java
@@ -6,6 +6,7 @@
 package ca.nines.ise;
 
 import ca.nines.ise.document.Annotations;
+import ca.nines.ise.document.Collations;
 import ca.nines.ise.log.Log;
 import java.io.File;
 
@@ -18,57 +19,23 @@ public class Tester {
   public static void main(String[] args) {
     Log log = Log.getInstance();
     try {
-      String in = data();
-      File f = new File("input/withTitlePage/Oth/apparatus/Oth_M_annotation.xml");
-      System.out.println("parsing.");
-      Annotations a = Annotations.builder().fromFile(f).build();
-      System.out.println("done.");
+      String in = "<collations>"
+              + "<coll>\n"
+              + "<l tln=\"3105\"/>\n"
+              + "<ln>5.4.119.1</ln>\n"
+              + "<lem resp=\"Capell 1767\">[He puts down the body.] <note>substantively</note></lem>\n"
+              + "<rdg resp=\"Capell 1767\">[<i>throwing down his Load.</i>]</rdg>\n"
+              + "<rdg resp=\"Weil and Weil\">[<i>He lays the body on the ground</i>]</rdg>\n"
+              + "<rdg resp=\"Kastan\">[<i>He drops Hotspur's body.</i>]</rdg>\n"
+              + "<rdg resp=\"Q1\"><note>not in Q1</note></rdg>\n"
+              + "</coll>"
+              + "</collations>";
+      Collations a = Collations.builder().fromString(in).build();
       System.out.println(a);
     } catch (Exception ex) {
       ex.printStackTrace(System.err);
     } finally {
       System.out.println(log);
     }
-  }
-
-  public static String data() {
-    return "<annotations>\n"
-            + "<!--Othello, commentary by Jessica Slights. Revised 2013-06-27-->\n"
-            + "\n"
-            + "<note>\n"
-            + "<ln tln=\"2\"/>\n"
-            + "<lem>Enter Roderigo and Iago.</lem>\n"
-            + "<level n=\"2\">\n"
-            + "The Q texts list Iago's name before Roderigo's. Playing the entrances in this order could operate as an early sign of the dominance that Iago has over Roderigo throughout the play.\n"
-            + "</level>\n"
-            + "</note>\n"
-            + "\n"
-            + "<note>\n"
-            + "<ln tln=\"4\"/>\n"
-            + "<lem>Tush,</lem>\n"
-            + "<level n=\"1\">\n"
-            + "Exclamation of impatience.\n"
-            + "</level>\n"
-            + "</note>\n"
-            + "\n"
-            + "<note>\n"
-            + "<ln tln=\"6\"/>\n"
-            + "<lem>this.</lem>\n"
-            + "<level n=\"1\">\n"
-            + "I.e. Othello's marriage to Desdemona.\n"
-            + "</level>\n"
-            + "<level n=\"2\">\n"
-            + "The referent seems deliberately unclear, and it becomes obvious only later in the scene that the two men are discussing Othello's marriage to Desdemona.\n"
-            + "</level>\n"
-            + "</note>\n"
-            + "\n"
-            + "<note>\n"
-            + "<ln tln=\"7\"/>\n"
-            + "<lem>'Sblood,</lem>\n"
-            + "<level n=\"1\">\n"
-            + "Contraction of \"God's blood,\" a common oath.\n"
-            + "</level>\n"
-            + "</note>"
-            + "</annotations>";
   }
 }
--- src/ca/nines/ise/node/lemma/Coll.java
+++ src/ca/nines/ise/node/lemma/Coll.java
@@ -5,15 +5,201 @@
  */
 package ca.nines.ise.node.lemma;
 
+import ca.nines.ise.util.BuilderInterface;
+import ca.nines.ise.util.XMLReader;
+import ca.nines.ise.util.XMLResourceReader;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import org.w3c.dom.Node;
+
 /**
  *
  * @author michael
  */
 public class Coll extends Lemma {
 
-  public Coll(String lem, String lineNumber, String node, String source, String tln, String xml) {
+  private final String lemResp;
+  private final String lemNote;
+  private final Map<String, String> readings;
+  private final Map<String, String> readingNotes;
+
+  public Coll(String lem,
+              String lineNumber,
+              String node,
+              String source,
+              String tln,
+              String xml,
+              String lemResp,
+              String lemNote,
+              Map<String, String> readings,
+              Map<String, String> readingNotes) {
     super(lem, lineNumber, node, source, tln, xml);
+    this.lemResp = lemResp;
+    this.lemNote = lemNote;
+    this.readings = new HashMap<>(readings);
+    this.readingNotes = new HashMap<>(readingNotes);
+  }
+  
+  public static CollBuilder builder() {
+    return new CollBuilder();
   }
 
+  public static class CollBuilder extends Lemma.LemmaBuilder
+          implements BuilderInterface<Coll> {
+
+    private String lemNote;
+    private String lemResp;
+    private final Map<String, String> readingNotes;
+    private final Map<String, String> readings;
+
+    public CollBuilder() {
+      super();
+      lemResp = "";
+      lemNote = "";
+      readings = new HashMap<>();
+      readingNotes = new HashMap<>();
+    }
+
+    public CollBuilder fromNode(Node in) throws ParserConfigurationException, XPathExpressionException {
+      return fromXML(in, new XMLResourceReader(in));
+    }
+
+    public CollBuilder fromString(Node in) throws ParserConfigurationException, XPathExpressionException {
+      return fromXML(new XMLResourceReader(in));
+    }
+
+    public CollBuilder fromXML(XMLReader xmlIn) throws XPathExpressionException {
+      setSource(xmlIn.getSource());
+      setLemResp(xmlIn.xpathString("lem/@resp"));
+      setLem(xmlIn.xpathString("lem/text()"));
+      String lnote = xmlIn.xpathString("lem/note/text()");
+      if( ! lnote.equals("")) {
+        setLemNote(lnote);
+      }
+      setTln(xmlIn.xpathString("l/@tln"));
+      setLineNumber(xmlIn.xpathString("ln/text()"));
+      for(Node n : xmlIn.xpathList("rdg")){
+        String rdg = xmlIn.xpathString("@resp", n);
+        addReading(rdg, xmlIn.xpathString("text()", n));
+        String note = xmlIn.xpathString("note/text()", n);
+        if( ! note.equals("")) {
+          addReadingNote(rdg, note);
+        }
+      }
+      return this;
+    }
+
+    public CollBuilder fromXML(Node in, XMLReader xmlIn) throws XPathExpressionException {
+      setSource(xmlIn.getSource());
+      setLemResp(xmlIn.xpathString("lem/@resp", in));
+      setLem(xmlIn.xpathString("lem/text()", in));
+      String lemNote = xmlIn.xpathString("lem/note//descendant::*/text()", in);
+      if( ! lemNote.equals("")) {
+        setLemNote(lemNote);
+      }
+      setTln(xmlIn.xpathString("l/@tln", in));
+      setLineNumber(xmlIn.xpathString("ln/text()", in));
+      for(Node n : xmlIn.xpathList("rdg", in)){
+        String rdg = xmlIn.xpathString("@resp", n);
+        addReading(rdg, xmlIn.xpathString("descendant::*/text()", n));
+        String note = xmlIn.xpathString("note/descendant::*/text()", n);
+        if( ! note.equals("")) {
+          addReadingNote(rdg, note);
+        }
+      }
+      return this;
+    }
+
+    public CollBuilder addReading(String resp, String reading) {
+      readings.put(resp, reading);
+      return this;
+    }
+
+    public CollBuilder addReadingNote(String resp, String readingNote) {
+      readingNotes.put(resp, readingNote);
+      return this;
+    }
+
+    @Override
+    public Coll build() {
+      return new Coll(lem, lineNumber, node, source, tln, xml, lemResp, lemNote, readings, readingNotes);
+    }
+
+    /**
+     * @param lemNote the lemNote to set
+     */
+    public CollBuilder setLemNote(String lemNote) {
+      this.lemNote = lemNote;
+      return this;
+    }
+
+    /**
+     * @param lemResp the lemResp to set
+     */
+    public CollBuilder setLemResp(String lemResp) {
+      this.lemResp = lemResp;
+      return this;
+    }
+
+  }
+
+  public String getReading(String resp) {
+    return readings.get(resp);
+  }
+
+  public String getReadingNote(String resp) {
+    return readingNotes.get(resp);
+  }
+
+  public boolean hasReading(String resp) {
+    return readings.containsKey(resp);
+  }
+
+  public boolean hasReadingNote(String resp) {
+    return readingNotes.containsKey(resp);
+  }
+
+  public String[] getReadings() {
+    String[] rdgs = readings.keySet().toArray(new String[readings.size()]);
+    Arrays.sort(rdgs);
+    return rdgs;
+  }
+
+  public String[] getReadingNotes() {
+    String[] rdgs = readingNotes.keySet().toArray(new String[readingNotes.size()]);
+    Arrays.sort(rdgs);
+    return rdgs;
+  }
+
+  /**
+   * @return the lemNote
+   */
+  public String getLemNote() {
+    return lemNote;
+  }
+
+  /**
+   * @return the lemResp
+   */
+  public String getLemResp() {
+    return lemResp;
+  }
+
+  @Override
+  public String toString() {
+    Formatter formatter = new Formatter();
+    formatter.format("%s%n", super.toString());
+    for (String rdg : getReadings()) {
+      formatter.format("    %s:%s%n", rdg, getReading(rdg));
+      if (hasReadingNote(rdg)) {
+        formatter.format("    note: %s%n", getReadingNote(rdg));
+      }
+    }
+    return formatter.toString();
+  }
 
 }
