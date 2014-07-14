6e895602b1de98a9ffdd3ec0a23d4f15efda2891
Michael Joyce <ubermichael@gmail.com>
2014-07-14 13:23:45 -0700

    I'm liking this new builder stuff.

--- src/ca/nines/ise/schema/Tag.java
+++ src/ca/nines/ise/schema/Tag.java
@@ -5,17 +5,18 @@
  */
 package ca.nines.ise.schema;
 
-import ca.nines.ise.util.XMLResourceReader;
-import java.io.IOException;
+import ca.nines.ise.schema.Attribute.AttributeBuilder;
+import ca.nines.ise.util.BuilderInterface;
+import ca.nines.ise.util.LocationData;
 import java.util.Arrays;
 import java.util.Formatter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
+import org.w3c.dom.NodeList;
 
 /**
  *
@@ -23,47 +24,140 @@ import org.xml.sax.SAXException;
  */
 public class Tag implements Comparable<Tag> {
 
-  // @TODO make the fields final and create a Builder.
-  
-  private Map<String, Attribute> attributes;
-  private String depreciated;
-  private String desc;
-  private String empty;
-  private String name;
+  private final Map<String, Attribute> attributes;
+  private final String depreciated;
+  private final String desc;
+  private final String empty;
 
-  private String where;
+  private final int lineNumber;
+  private final String name;
+  private final String source;
 
-  public Tag(String in) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
-    XMLResourceReader xmlIn = new XMLResourceReader(in);
-    name = xmlIn.xpathString("@name");
-    depreciated = xmlIn.xpathString("@depreciated");
-    where = xmlIn.xpathString("@where");
-    empty = xmlIn.xpathString("@empty");
-    desc = xmlIn.xpathString("desc/text()");
+  public static class TagBuilder implements BuilderInterface<Tag> {
 
-    attributes = new HashMap<>();
-    for (Node n : xmlIn.xpathList("attribute")) {
-      Attribute attr = new Attribute(n, xmlIn);
-      attributes.put(attr.getName().toLowerCase(), attr);
+    private Map<String, Attribute> attributes;
+    private String depreciated;
+    private String desc;
+    private String empty;
+
+    private int lineNumber;
+    private String name;
+    private String source;
+
+    private TagBuilder() {
+      attributes = new HashMap<>();
+      depreciated = "";
+      desc = "";
+      empty = "";
+      name = "";
+      lineNumber = 0;
+      source = "";
     }
+
+    public void addAttribute(Attribute attr) {
+      attributes.put(attr.getName(), attr);
+    }
+
+    @Override
+    public Tag build() {
+      return new Tag(name, desc, empty, depreciated, attributes, source, lineNumber);
+    }
+
+    public TagBuilder from(Node n) {
+      NamedNodeMap map = n.getAttributes();
+      Node tmp;
+
+      LocationData loc = (LocationData) n.getUserData(LocationData.LOCATION_DATA_KEY);
+      setSource(loc.getSystemId());
+      setLineNumber(loc.getStartLine());
+
+      setName(map.getNamedItem("name").getTextContent());
+      
+      tmp = map.getNamedItem("empty");
+      if (tmp != null) {
+        setEmpty(tmp.getTextContent());
+      }
+
+      tmp = map.getNamedItem("depreciated");
+      if (tmp != null) {
+        setDepreciated(tmp.getTextContent());
+      }
+
+      setDesc(((Element) n).getElementsByTagName("desc").item(0).getTextContent());
+
+      NodeList list = ((Element) n).getElementsByTagName("attribute");
+      int length = list.getLength();
+      for (int i = 0; i < length; i++) {
+        AttributeBuilder ab = Attribute.builder();
+        addAttribute(ab.from(list.item(i)).build());
+      }
+
+      return this;
+    }
+
+    /**
+     * @param attributes the attributes to set
+     */
+    public void setAttributes(Map<String, Attribute> attributes) {
+      this.attributes = new HashMap<>(attributes);
+    }
+
+    /**
+     * @param depreciated the depreciated to set
+     */
+    public void setDepreciated(String depreciated) {
+      this.depreciated = depreciated;
+    }
+
+    /**
+     * @param desc the desc to set
+     */
+    public void setDesc(String desc) {
+      this.desc = desc;
+    }
+
+    /**
+     * @param empty the empty to set
+     */
+    public void setEmpty(String empty) {
+      this.empty = empty;
+    }
+
+    /**
+     * @param lineNumber the lineNumber to set
+     */
+    public void setLineNumber(int lineNumber) {
+      this.lineNumber = lineNumber;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    /**
+     * @param source the source to set
+     */
+    public void setSource(String source) {
+      this.source = source;
+    }
+
   }
 
-  public Tag(Node in) throws XPathExpressionException, ParserConfigurationException {
-    this(in, new XMLResourceReader(in));
+  public static TagBuilder builder() {
+    return new TagBuilder();
   }
 
-  public Tag(Node in, XMLResourceReader xmlIn) throws XPathExpressionException {
-    name = xmlIn.xpathString("@name", in);
-    depreciated = xmlIn.xpathString("@depreciated", in);
-    where = xmlIn.xpathString("@where", in);
-    empty = xmlIn.xpathString("@empty", in);
-    desc = xmlIn.xpathString("desc/text()", in);
-
-    attributes = new HashMap<>();
-    for (Node n : xmlIn.xpathList("attribute", in)) {
-      Attribute attr = new Attribute(n, xmlIn);
-      attributes.put(attr.getName().toLowerCase(), attr);
-    }
+  private Tag(String name, String desc, String empty, String depreciated, Map<String, Attribute> attributes, String source, int lineNumber) {
+    this.name = name;
+    this.desc = desc;
+    this.empty = empty;
+    this.depreciated = depreciated;
+    this.attributes = new HashMap<>(attributes);
+    this.source = source;
+    this.lineNumber = lineNumber;
   }
 
   @Override
@@ -85,11 +179,11 @@ public class Tag implements Comparable<Tag> {
     return names;
   }
 
-    public Attribute[] getAttributes() {
-      Attribute[] a = attributes.values().toArray(new Attribute[attributes.size()]);
-      Arrays.sort(a);
-      return a;
-    }
+  public Attribute[] getAttributes() {
+    Attribute[] a = attributes.values().toArray(new Attribute[attributes.size()]);
+    Arrays.sort(a);
+    return a;
+  }
 
   /**
    * @return the depreciated
@@ -113,6 +207,13 @@ public class Tag implements Comparable<Tag> {
   }
 
   /**
+   * @return the lineNumber
+   */
+  public int getLineNumber() {
+    return lineNumber;
+  }
+
+  /**
    * @return the name
    */
   public String getName() {
@@ -120,13 +221,10 @@ public class Tag implements Comparable<Tag> {
   }
 
   /**
-   * @return the where
+   * @return the source
    */
-  public String getWhere() {
-    if (where.equals("")) {
-      return "anywhere";
-    }
-    return where;
+  public String getSource() {
+    return source;
   }
 
   public boolean isDepreciated() {
@@ -144,14 +242,15 @@ public class Tag implements Comparable<Tag> {
   @Override
   public String toString() {
     Formatter formatter = new Formatter();
-    
-    formatter.format("%s:%s:%s%n", name, depreciated, where);
+
+    formatter.format("%s:%s%n", source, lineNumber);
+    formatter.format("%s:%s:%s%n", name, empty, depreciated);
     Iterator<Attribute> i = attributes.values().iterator();
     while (i.hasNext()) {
       Attribute a = i.next();
       formatter.format("%s", a);
     }
-    
+
     return formatter.toString();
   }
 }
