ada5f771379d72ccbee03d61ea2ed0d1df8a975e
Michael Joyce <ubermichael@gmail.com>
2014-07-14 18:48:45 -0700

    Hey, this could actually work.

--- src/ca/nines/ise/cmd/Transform.java
+++ src/ca/nines/ise/cmd/Transform.java
@@ -9,6 +9,7 @@ import ca.nines.ise.dom.DOM;
 import ca.nines.ise.dom.DOMBuilder;
 import ca.nines.ise.log.Log;
 import ca.nines.ise.output.Output;
+import ca.nines.ise.output.RTFOutput;
 import ca.nines.ise.output.TextOutput;
 import ca.nines.ise.output.XMLOutput;
 import java.io.File;
@@ -32,7 +33,7 @@ public class Transform extends Command {
 
   @Override
   public String description() {
-    return "Transform an ISE SGML document to XML.";
+    return "Transform an ISE SGML document another format.";
   }
 
   @Override
@@ -42,9 +43,10 @@ public class Transform extends Command {
     try {
       Log log = Log.getInstance();
       Locale.setDefault(Locale.ENGLISH);
+      
       out = new PrintStream(System.out, true, "UTF-8");
-      if (cmd.hasOption("l")) {
-        out = new PrintStream(new FileOutputStream(cmd.getOptionValue("l")), true, "UTF-8");
+      if (cmd.hasOption("o")) {
+        out = new PrintStream(new FileOutputStream(cmd.getOptionValue("o")), true, "UTF-8");
       }
 
       String[] files = getArgList(cmd);
@@ -65,6 +67,9 @@ public class Transform extends Command {
       }
       if (cmd.hasOption("xml")) {
         renderer = new XMLOutput(out);
+      }
+      if(cmd.hasOption("rtf")) {
+        renderer = new RTFOutput(out);
       }
 
       if (renderer != null) {
@@ -92,6 +97,7 @@ public class Transform extends Command {
     opts.addOption("o", true, "Send output to file.");
     opts.addOption("xml", false, "Transform output to XML.");
     opts.addOption("text", false, "Transform output to UTF-8 (unicode) text.");
+    opts.addOption("rtf", false, "Transform output to an RTF document.");
     return opts;
   }
 
