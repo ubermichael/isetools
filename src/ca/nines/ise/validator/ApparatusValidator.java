4581fdbfc01d5f895701c35bf150c5c258b00edf
Michael Joyce <ubermichael@gmail.com>
2014-07-17 15:56:17 -0700

    begin validating notes.

--- src/ca/nines/ise/validator/ApparatusValidator.java
+++ src/ca/nines/ise/validator/ApparatusValidator.java
@@ -7,7 +7,12 @@
 package ca.nines.ise.validator;
 
 import ca.nines.ise.document.Apparatus;
+import ca.nines.ise.dom.DOM;
+import ca.nines.ise.log.Log;
+import ca.nines.ise.log.Message;
 import ca.nines.ise.node.lemma.Lemma;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -16,4 +21,31 @@ import ca.nines.ise.node.lemma.Lemma;
  */
 abstract public class ApparatusValidator<T extends Apparatus>{
   
+  private static final Pattern singleTlnPattern = Pattern.compile("\\p{Digit}+(?:\\.\\p{Digit}+)?");
+  private static final Pattern splitTlnPattern = Pattern.compile("\\p{Digit}+-\\p{Digit}+");
+  
+  public void validate(Lemma lem) {
+    Matcher matcher;
+    String cName = lem.getClass().getSimpleName();
+
+    matcher = singleTlnPattern.matcher(lem.getTln());
+    if(matcher.matches()) {
+      return;
+    }
+    
+    matcher = splitTlnPattern.matcher(lem.getTln());
+    if(matcher.matches()) {
+      return;
+    }
+    
+    Message m = Message.builder("validator." + this.getClass().getSimpleName().toLowerCase() + ".bad_tln")
+            .fromLemma(lem)
+            .addNote("The invalid TLN looks like " + lem.getTln())
+            .build();
+    Log.addMessage(m);
+  }
+  
+  public void validate(DOM dom, Lemma lem) {
+    
+  }
 }
