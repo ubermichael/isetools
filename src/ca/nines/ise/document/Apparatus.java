4cedb6fbeedda0af409080c04b21b93da31fb40c
Michael Joyce <ubermichael@gmail.com>
2014-07-21 14:42:20 -0700

    index the notes (and eventually dolls).

--- src/ca/nines/ise/document/Apparatus.java
+++ src/ca/nines/ise/document/Apparatus.java
@@ -14,13 +14,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 package ca.nines.ise.document;
 
 import ca.nines.ise.node.lemma.Lemma;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -33,32 +34,55 @@ abstract public class Apparatus<T extends Lemma> implements Iterable<T> {
 
   private final List<T> lemmas;
   private final String source;
+  private final Map<String, List<T>> index;
 
   public abstract static class ApparatusBuilder<T extends Lemma> {
+
     protected List<T> lemmas;
     protected String source;
-    
+
     public ApparatusBuilder() {
       lemmas = new ArrayList<>();
       source = "";
     }
-    
+
     public void addLemma(T t) {
       lemmas.add(t);
     }
-    
+
     public void setSource(String source) {
       this.source = source;
     }
   }
-  
+
+  private void addToIndex(String tln, T lemma) {
+    if (index.get(tln) == null) {
+      index.put(tln, new ArrayList<T>());
+    }
+    index.get(tln).add(lemma);
+  }
+
   protected Apparatus(String source, List<T> lemmas) {
     this.source = source;
     this.lemmas = new ArrayList<>(lemmas);
+    this.index = new HashMap<>();
+
+    for (T lemma : lemmas) {
+      if (lemma.isTlnSplit()) {
+        addToIndex(lemma.getTlnStart(), lemma);
+        addToIndex(lemma.getTlnEnd(), lemma);        
+      } else {
+        addToIndex(lemma.getTln(), lemma);        
+      }
+    }
   }
-  
+
   public T get(int i) {
     return lemmas.get(i);
+  }
+  
+  public List<T> get(String tln) {
+    return new ArrayList<>(index.get(tln));
   }
 
   /**
@@ -72,15 +96,15 @@ abstract public class Apparatus<T extends Lemma> implements Iterable<T> {
   public Iterator<T> iterator() {
     return lemmas.iterator();
   }
-  
+
   public int size() {
     return lemmas.size();
   }
-  
+
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    for(Lemma lem : lemmas) {
+    for (Lemma lem : lemmas) {
       sb.append(lem.toString());
       sb.append("\n");
     }
