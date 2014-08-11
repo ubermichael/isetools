6285e8181766481051a206530b8db184ab204fe5
Michael Joyce <ubermichael@gmail.com>
2014-08-11 14:51:21 -0700

    Validate tag nesting.

--- src/ca/nines/ise/cmd/Validate.java
+++ src/ca/nines/ise/cmd/Validate.java
@@ -23,6 +23,7 @@ import ca.nines.ise.log.Log;
 import ca.nines.ise.log.Message;
 import ca.nines.ise.schema.Schema;
 import ca.nines.ise.validator.DOMValidator;
+import ca.nines.ise.validator.NestingValidator;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
@@ -51,7 +52,8 @@ public class Validate extends Command {
     Log log = Log.getInstance();
     Locale.setDefault(Locale.ENGLISH);
     Schema schema = Schema.defaultSchema();
-    DOMValidator validator = new DOMValidator();
+    DOMValidator dv = new DOMValidator();
+    NestingValidator nv = new NestingValidator();
     PrintStream out = new PrintStream(System.out, true, "UTF-8");
 
     if (cmd.hasOption("l")) {
@@ -64,7 +66,8 @@ public class Validate extends Command {
       for (File in : files) {
         DOM dom = new DOMBuilder(in).build();
         if (dom.getStatus() != DOM.DOMStatus.ERROR) {
-          validator.validate(dom, schema);
+          dv.validate(dom, schema);
+          nv.validate(dom);
         } else {
           Message m = Message.builder("dom.errors")
                   .setSource(dom.getSource())
---
+++ src/ca/nines/ise/validator/NestingValidator.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2014 Michael Joyce <ubermichael@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package ca.nines.ise.validator;
+
+import ca.nines.ise.annotation.ErrorCode;
+import ca.nines.ise.dom.DOM;
+import ca.nines.ise.log.Log;
+import ca.nines.ise.log.Message;
+import ca.nines.ise.node.EndNode;
+import ca.nines.ise.node.Node;
+import ca.nines.ise.node.StartNode;
+import java.util.ArrayDeque;
+
+/**
+ *
+ * @author Michael Joyce <ubermichael@gmail.com>
+ */
+public class NestingValidator {
+
+  ArrayDeque<StartNode> nodeStack;
+
+@ErrorCode(code = {
+  "validator.nesting.split",
+  "validator.nesting.missing_start_tag"
+})
+  private void process_end(EndNode n) {
+    if (nodeStack.peekFirst().getName().toLowerCase().equals(n.getName().toLowerCase())) {
+      nodeStack.pop();
+      return;
+    }
+
+    Message m = Message.builder("validator.nesting.split")
+            .fromNode(n)
+            .addNote("Tag " + n.getName() + " splits other tags.")
+            .build();
+    Log.addMessage(m);
+
+    // this is a split tag.
+    ArrayDeque<StartNode> splitStack = new ArrayDeque<>();
+    while (nodeStack.size() >= 1) {
+      StartNode start = nodeStack.pop();
+      if (start.getName().toLowerCase().equals(n.getName().toLowerCase())) {
+        break; // while.
+      }
+      splitStack.push(start);
+    }
+
+    if (nodeStack.isEmpty()) {
+      m = Message.builder("validator.nesting.missing_start_tag")
+              .fromNode(n)
+              .addNote("Cannot find start tag that corresponds to end tag " + n.getName())
+              .build();
+      Log.addMessage(m);
+    }
+
+    while (splitStack.size() >= 1) {
+      nodeStack.push(splitStack.pop());
+    }
+
+  }
+
+  @ErrorCode(code = {
+    "validator.nesting.recursive"
+  })
+  private void process_start(StartNode n) {
+    for (StartNode s : nodeStack) {
+      if (s.getName().toLowerCase().equals(n.getName().toLowerCase())) {
+        Message m = Message.builder("validator.nesting.recursive")
+                .fromNode(n)
+                .addNote("Tag " + n.getName() + " cannot be recursive.")
+                .addNote("Or the tag " + s.getName() + " at TLN " + s.getTLN() + " on line " + s.getLine() + " may be unclosed.")
+                .build();
+        Log.addMessage(m);
+      }
+    }
+    nodeStack.push(n);
+  }
+
+  public void validate(DOM dom) {
+    nodeStack = new ArrayDeque<>();
+
+    for (Node n : dom) {
+      switch (n.type()) {
+        case END:
+          process_end((EndNode) n);
+          break;
+        case START:
+          process_start((StartNode) n);
+          break;
+      }
+    }
+
+    for(StartNode n : nodeStack) {
+        Message m = Message.builder("validator.nesting.unclosed")
+                .fromNode(n)
+                .addNote("Start tag " + n.getName() + " has no matching end tag")
+                .build();
+        Log.addMessage(m);
+    }
+    
+  }
+
+}
