7011c1046b7398fafe9edfed173f5f1511277282
imoutits <thats.it.89@gmail.com>
2015-04-01 22:20:07 -0700

    forgot to add these...

---
+++ src/main/java/ca/nines/ise/constants/IML.java
@@ -0,0 +1,37 @@
+package ca.nines.ise.constants;
+
+public class IML {
+	  public static final String QLN = "QLN";
+	  public static final String WLN = "WLN";
+	  public static final String TLN = "TLN";
+	  public static final String LINK = "LINK";
+	  public static final String META = "META";
+	  public static final String RULE = "RULE";
+	  public static final String SPACE = "SPACE";
+	  public static final String BR = "BR";
+	  public static final String COL = "COL";
+	  public static final String ISEHEADER = "ISEHEADER";
+	  public static final String SP = "SP";
+	  public static final String LINEGROUP = "LINEGROUP";
+	  public static final String MARG = "MARG";
+	  public static final String TITLEHEAD = "TITLEHEAD";
+	  public static final String QUOTE = "QUOTE";
+	  public static final String WORK = "WORK";
+	  public static final String PAGE = "PAGE";
+	  public static final String LINE = "L";
+	  public static final String RA = "RA";
+	  public static final String C = "C";
+	  public static final String J = "J";
+	  public static final String ROOT = "root";
+	  public static final String ACT = "ACT";
+	  public static final String SCENE = "SCENE";
+	  public static final String MODE = "MODE";
+	  public static final String S = "S";
+	  public static final String AMBIG = "AMBIG";
+	  public static final String RDG = "RDG";
+	  public static final String PROP = "PROP";
+	  
+	  public static final String[] LINE_CHILDREN = {"ABBR","AMBIG","BLL","CL","EM","FOREIGN","HW","ORNAMENT",
+		  					"LD","LS","PROP","R","S","SC","SD","SWASH",TITLEHEAD,TLN,QLN,WLN};
+	  public static final String[] ALIGNMENT = {"RA","C","J"};
+}
---
+++ src/main/java/ca/nines/ise/constants/XML.java
@@ -0,0 +1,35 @@
+package ca.nines.ise.constants;
+
+public class XML {
+	  public static final String[] ALIGNMENT ={"right","center","justify"};
+	  public static final String QLN = "qln";
+	  public static final String WLN = "wln";
+	  public static final String TLN = "tln";
+	  public static final String LINK = "link";
+	  public static final String META = "meta";
+	  public static final String RULE = "rule";
+	  public static final String SPACE = "space";
+	  public static final String BR = "br";
+	  public static final String COL = "col";
+	  public static final String ISEHEADER = "iseheader";
+	  public static final String SP = "sp";
+	  public static final String LINEGROUP = "linegroup";
+	  public static final String MARG = "marg";
+	  public static final String TITLE = "title";
+	  public static final String QUOTE = "quote";
+	  public static final String WORK = "work";
+	  public static final String PAGE = "page";
+	  public static final String LINE = "l";
+	  public static final String RA = "ra";
+	  public static final String C = "c";
+	  public static final String J = "j";
+	  public static final String S = "s";
+	  public static final String Q = "q";
+	  public static final String MS = "ms";
+	  public static final String ACT = "act";
+	  public static final String SCENE = "scene";
+	  public static final String MODE = "mode";
+	  public static final String AMBIG = "ambig";
+	  public static final String RDG = "rdg";
+	  public static final String PROP = "prop";
+}
