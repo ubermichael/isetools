35d73b3371b1f66ab8223e54a32b8c7f6aeba3bf
imoutits <malcolm.moran@outlook.com>
2015-07-06 04:41:04 -0700

    started validator for HW tags

---
+++ src/main/java/ca/nines/ise/validator/HungWordValidator.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2015 Malcolm Moran <malcolm.moran@outlook.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package ca.nines.ise.validator;
+
+import ca.nines.ise.annotation.ErrorCode;
+import ca.nines.ise.dom.DOM;
+import ca.nines.ise.log.Log;
+import ca.nines.ise.log.Message;
+import ca.nines.ise.node.EndNode;
+import ca.nines.ise.node.Node;
+import ca.nines.ise.node.NodeType;
+/**
+ * 
+ * @author Malcolm Moran <malcolm.moran@outlook.com>
+ * 
+ * Ensures HW tags do not span more than one line, 
+ * and are always used at the end of a line.
+ */
+public class HungWordValidator {
+
+  @ErrorCode(code = {
+    "validator.hungWord.spannedLines",
+    "validator.hungWord.endOfLine"
+  })
+  private void process_end(EndNode n) {
+    if (n.getText().contains("\n")){
+      Message m = Message.builder("validator.spanLine.spannedLines")
+          .fromNode(n)
+          .addNote("Tag " + n.getName() + " spans more than one line")
+          .build();
+      Log.addMessage(m);
+    }
+  }
+
+  public void validate(DOM dom) {
+    for (Node n : dom) {
+      if (n.type() == NodeType.END) {
+        switch(n.getName().toLowerCase()){
+          case "hw":
+            process_end((EndNode) n);
+            break;
+        }
+      }
+    }
+  }
+
+}
