fe3e9bc4518b8fe11ffbbbb0928b8b0b5a6015a9
imoutits <malcolm.moran@outlook.com>
2015-10-23 09:55:44 -0700

    added functionality to use new schema attributes

--- src/main/java/ca/nines/ise/schema/Schema.java
+++ src/main/java/ca/nines/ise/schema/Schema.java
@@ -22,8 +22,10 @@ import ca.nines.ise.util.XMLDriver;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import javax.xml.parsers.ParserConfigurationException;
@@ -45,17 +47,6 @@ import org.xml.sax.SAXException;
  * @author Michael Joyce <ubermichael@gmail.com>
  */
 public class Schema {
-  /* elements in which text should not be parsed in parse_text() */
-  public static final String[] DONT_PARSE_TEXT = { "cw", "sig", "rt", "pn",
-	  "label" };
-  public static final String[] LINE_PARENTS = { "work", "marg", "bracegroup",
-	  "splitline", "quote", "linegroup" };
-  public static final String[] TYPEFACES = { "bll", "r" };
-  public static final String[] FLATTEN = { "font" };
-  public static final String[] INLINE_TAGS = { "ABBR", "BLL", "CL", "EM",
-			"FONT", "FOREIGN", "HW", "I", "LD", "LS", "ORNAMENT", "PERSON",
-			"PLACE", "PROP", "Q", "R", "S", "SC", "SD", "SUB", "SUP", "SWASH",
-			"TITLE" };
   /**
    * The edition for the schema
    */
@@ -264,6 +255,58 @@ public class Schema {
       sb.append(t);
     }
     return sb.toString();
+  }
+  
+
+  public List<String>  get_Inline_tags() {
+    List<String> list = new ArrayList<String>();
+    Tag[] tags = getTags();
+    for(int i=0; i<tags.length; i++){
+      if (tags[i].getXMLInline().equals("yes")){
+        list.add(tags[i].getName());
+      }
+    }
+    return list;
+  }
+  public List<String>  get_flatten_tags() {
+    List<String> list = new ArrayList<String>();
+    Tag[] tags = getTags();
+    for(int i=0; i<tags.length; i++){
+      if (tags[i].getXMLFlatten().equals("yes")){
+        list.add(tags[i].getName());
+      }
+    }
+    return list;
+  }
+  public List<String>  get_typeface_tags() {
+    List<String> list = new ArrayList<String>();
+    Tag[] tags = getTags();
+    for(int i=0; i<tags.length; i++){
+      if (tags[i].getXMLTypeface().equals("yes")){
+        list.add(tags[i].getName());
+      }
+    }
+    return list;
+  }
+  public List<String>  get_line_parent_tags() {
+    List<String> list = new ArrayList<String>();
+    Tag[] tags = getTags();
+    for(int i=0; i<tags.length; i++){
+      if (tags[i].getXMLLineParent().equals("yes")){
+        list.add(tags[i].getName());
+      }
+    }
+    return list;
+  }
+  public List<String>  get_unparsed_text_tags() {
+    List<String> list = new ArrayList<String>();
+    Tag[] tags = getTags();
+    for(int i=0; i<tags.length; i++){
+      if (tags[i].getXMLUnparsedText().equals("yes")){
+        list.add(tags[i].getName());
+      }
+    }
+    return list;
   }
 
 }
