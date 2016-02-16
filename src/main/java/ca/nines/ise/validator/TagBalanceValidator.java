67d6b7a45cf713af8ab431a11ff6c3c404952463
emmental <malcolm.moran@outlook.com>
2016-02-15 18:00:43 -0800

    forgot to add validator

---
+++ src/main/java/ca/nines/ise/validator/TagBalanceValidator.java
@@ -0,0 +1,82 @@
+package ca.nines.ise.validator;
+
+import ca.nines.ise.annotation.ErrorCode;
+import ca.nines.ise.dom.DOM;
+import ca.nines.ise.log.Log;
+import ca.nines.ise.log.Message;
+import ca.nines.ise.node.EndNode;
+import ca.nines.ise.node.Node;
+import ca.nines.ise.node.StartNode;
+import ca.nines.ise.schema.Schema;
+import java.util.ArrayDeque;
+
+public class TagBalanceValidator {
+  private final Schema schema;
+
+  public TagBalanceValidator(Schema schema) {
+      this.schema = schema;
+  }
+  
+  ArrayDeque<StartNode> nodeStack;
+  
+  @ErrorCode(code = {
+    "validator.nesting.missing_start_tag"
+  })
+  private void process_end(EndNode n) {
+    if (nodeStack.peekFirst().getName().toLowerCase().equals(n.getName().toLowerCase())) {
+      nodeStack.pop();
+      return;
+    }
+  
+    // this is a split tag.
+    ArrayDeque<StartNode> splitStack = new ArrayDeque<>();
+    while (nodeStack.size() >= 1) {
+      StartNode start = nodeStack.peek();
+      if (start.getName().toLowerCase().equals(n.getName().toLowerCase())) {
+        break; // while.
+      }
+      splitStack.push(nodeStack.pop());
+    }
+  
+    if (nodeStack.isEmpty()) {
+      Message m = Message.builder("validator.TagBalance.missing_start")
+              .fromNode(n)
+              .addNote("Cannot find start tag that corresponds to end tag " + n.getName())
+              .build();
+      Log.addMessage(m);
+    }
+  
+    while (splitStack.size() >= 1) {
+      nodeStack.push(splitStack.pop());
+    }
+  
+  }
+  
+  private void process_start(StartNode n) {
+    nodeStack.push(n);
+  }
+
+  public void validate(DOM dom) {
+    nodeStack = new ArrayDeque<>();
+  
+    for (Node n : dom) {
+      switch (n.type()) {
+        case END:
+          process_end((EndNode) n);
+          break;
+        case START:
+          process_start((StartNode) n);
+          break;
+      }
+    }
+  
+    for (StartNode n : nodeStack) {
+      Message m = Message.builder("validator.tagBalance.unclosed")
+              .fromNode(n)
+              .addNote("Start tag " + n.getName() + " has no matching end tag")
+              .build();
+      Log.addMessage(m);
+    }
+  
+  }
+}
