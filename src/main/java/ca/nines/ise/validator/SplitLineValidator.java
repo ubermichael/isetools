d2abc9fc63a8347ee24cde02574cdc7a50a52d61
emmental <malcolm.moran@outlook.com>
2016-03-01 04:31:29 -0800

    SplitLineValidator now checks to see if sectioning tags are crossing a splitline. Also added an error if middle lines don't have a part="m"

--- src/main/java/ca/nines/ise/validator/SplitLineValidator.java
+++ src/main/java/ca/nines/ise/validator/SplitLineValidator.java
@@ -16,6 +16,7 @@ public class SplitLineValidator {
   public SplitLineValidator() {}
   
 EmptyNode current;
+ArrayDeque<Node> nodeStack;
 
 @ErrorCode(code = {
     "validator.splitLine.recursive"
@@ -24,12 +25,14 @@ private void process_start(EmptyNode n) {
     if (current != null) {
         Message m = Message.builder("validator.splitLine.recursive")
                .fromNode(n)
-               .addNote(get_name(n) + " cannot start a new split line (part=\"i\") before the current one ("+get_name(current)+" @ TLN="+current.getTLN()+") is finished")
+               .addNote(get_name(n) + " cannot start a new split line (@part=\"i\") before the current one ("+get_name(current)+" @ TLN="+current.getTLN()+") is finished")
                .build();
         Log.addMessage(m);
     }
-    else
+    else{
       current = n;
+      nodeStack.push(current);
+    }
 }
 
 @ErrorCode(code = {
@@ -39,24 +42,36 @@ private void process_middle(EmptyNode n) {
   if (current == null){
     Message m = Message.builder("validator.splitLine.notStartedM")
         .fromNode(n)
-        .addNote(get_name(n) + " has a part=\"m\" but no matching \"i\" L tag")
+        .addNote(get_name(n) + " has a @part=\"m\" but no matching \"i\" L tag")
         .build();
     Log.addMessage(m);
   }
 }
 
 @ErrorCode(code = {
-  "validator.splitLine.notStartedF"
+  "validator.splitLine.notStartedF",
+  "validator.partLine.crossing"
 })
 private void process_finish(EmptyNode n) {
   if (current == null){
     Message m = Message.builder("validator.splitLine.notStartedF")
             .fromNode(n)
-            .addNote(get_name(n) + " has a part=\"f\" but no matching \"i\" L tag")
+            .addNote(get_name(n) + " has a @part=\"f\" but no matching \"i\" L tag")
             .build();
     Log.addMessage(m);
-  }else 
+  //if there's a sectioning open in the split line, it's crossing
+  }else if (!nodeStack.peekFirst().getName().toLowerCase().equals("l")){
+    Message m = Message.builder("validator.partLine.crossing")
+        .fromNode(n)
+        .addNote("Tag " + nodeStack.peekFirst().getName() + " crosses a split-line ("+get_name(current)+" @ TLN="+current.getTLN()+")")
+        .build();
+    Log.addMessage(m);
+    nodeStack.remove(current);
     current = null;
+  }else{
+    nodeStack.remove(current);
+    current = null;
+  }
 }
 
 
@@ -69,33 +84,91 @@ private String get_name(EmptyNode n){
 }
 
 @ErrorCode(code = {
-  "validator.splitLine.unclosed"
+    "validator.partLine.crossing"
+})
+private void process_sectioning_end(EndNode n) {
+  //if closing itself with nothing in between, we're good
+  if (nodeStack.peekFirst().getName().toLowerCase().equals(n.getName().toLowerCase())) {
+    nodeStack.pop();
+    return;
+  }
+  //if closing with splitline open, error
+  if (current != null) {
+    Message m = Message.builder("validator.partLine.crossing")
+      .fromNode(n)
+      .addNote("Tag " + n.getName() + " crosses a split-line ("+get_name(current)+" @ TLN="+current.getTLN()+")")
+      .build();
+    Log.addMessage(m);
+  }
+  //remove n tag from stack
+  Node end = null;
+  for (Node s : nodeStack) {
+    if (s.getName().toLowerCase().equals(n.getName().toLowerCase()))
+      end = s;
+  }
+  if (end != null)
+    nodeStack.remove(end);
+}
+
+private void process_sectioning_start(StartNode n){
+  nodeStack.push(n);
+}
+
+@ErrorCode(code = {
+  "validator.splitLine.unclosed",
+  "validator.splitLine.missingPart"
 })
 public void validate(DOM dom) {
   current = null;
+  nodeStack = new ArrayDeque<>();
 
-  for (Node n : dom) {
-    if (n.getName().toLowerCase().equals("l")){
-      EmptyNode en = (EmptyNode) n;
-      if (en.hasAttribute("part")){
-        switch (en.getAttribute("part")) {
-          case "i":
-            process_start(en);
+  for (Node n : dom) {      
+    switch (n.getName().toLowerCase()){
+      case "act":
+      case "backmatter":
+      case "div":
+      case "frontmatter":
+      case "marg":
+      case "page":
+      case "scene":
+      case "stanza":
+        switch (n.type()) {
+          case END:
+            process_sectioning_end((EndNode) n);
             break;
-          case "m":
-            process_middle(en);
+          case START:
+            process_sectioning_start((StartNode) n);
             break;
-          case "f":
-            process_finish(en);
+        }
+        break;
+      case "l":
+        EmptyNode en = (EmptyNode) n;
+        if (en.hasAttribute("part")){
+          switch (en.getAttribute("part")) {
+            case "i":
+              process_start(en);
+              break;
+            case "m":
+              process_middle(en);
+              break;
+            case "f":
+              process_finish(en);
+          }
+        }else if (current != null){
+          // line in a splitline but doesn't have a part attribute
+          Message m = Message.builder("validator.splitLine.missingPart")
+              .fromNode(n)
+              .addNote(get_name(en) + " doesn't have a @part attribute but is within a split line ("+get_name(current)+" @ TLN="+current.getTLN()+")")
+              .build();
+          Log.addMessage(m);
         }
       }
-    } 
   }
 
   if (current != null) {
     Message m = Message.builder("validator.splitLine.unclosed")
             .fromNode(current)
-            .addNote(get_name(current) + " has a part=\"i\" but no matching \"f\" L tag")
+            .addNote(get_name(current) + " has a @part=\"i\" but no matching \"f\" L tag")
             .build();
     Log.addMessage(m);
   }
